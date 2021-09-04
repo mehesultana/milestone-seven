7be66c58565c63568e74bbf13c52d105cfc44ffa
mehesultana <sultanamehe01@gmail.com>
2021-09-05 01:35:15 +0600

    assign, reload

---
+++ 38-browser-api-and-methods/location.js
@@ -0,0 +1,7 @@
+// 38-3 Location, URL parts, query string, href, hash, assign, reload
+const refreshPage = () => {
+    location.reload();
+};
+const visitPH = () => {
+    location.assign('https://web.programming-hero.com/');
+};
