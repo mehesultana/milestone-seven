f5bb4607c1aca3dd0007ff52a3fed4fe8b612adf
mehesultana <sultanamehe01@gmail.com>
2021-09-05 00:19:22 +0600

    BOM vs DOM

---
+++ 38-browser-api-and-methods/explore.js
@@ -0,0 +1,13 @@
+// 38-1 Module Overview, BOM vs DOM, Browser API
+const name = 15;
+const student = {
+    name: 'Jolil',
+    friend: 'borsha',
+    job: 'Business',
+    makeMovie: function (name) {
+        console.log(name);
+    },
+};
+student.makeMovie('Din raat');
+// document.getElementById('name');
+alert('abc');
