aad5ac55a78b9f959b47d3268a5a0747485ae8b2
mehesultana <sultanamehe01@gmail.com>
2021-09-06 01:33:42 +0600

    incomplete

---
+++ 38.5-browser-api-practice/prompt.js
@@ -0,0 +1,10 @@
+// incomplete
+const askMoney = (num) => {
+    const money = prompt('How much money do you have ?');
+    if (money === true) {
+        const result = num + 200;
+        return result;
+    }
+};
+//const total = askMoney(100);
+//console.log(total);
