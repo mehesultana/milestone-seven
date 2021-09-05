5b0b971c28ea8760809f620163f33afaaa481bde
mehesultana <sultanamehe01@gmail.com>
2021-09-06 01:32:51 +0600

    practice

---
+++ 38.5-browser-api-practice/browserPractice.js
@@ -0,0 +1,18 @@
+// 38_5-1 Practice Browser api with checklist and a must work
+
+// 1. show output after 3.5 seconds
+console.log('Boreddddd');
+console.log('I need a tourrrr :)');
+setTimeout(() => console.log('aww jaiii *>_<*'), 3500);
+console.log('volagonj niya geleo colbeee :)');
+
+// 2. take a number using prompt . add 200 . then alert the result
+
+// 3. do you want to see the href. if ok console log href
+// 4. purpose of cookies. 5-7 lines in english
+// 5. 3 different between local storage and session storage
+
+// ---- HOMEWORK ----
+// 6. how javascript run ? 5-7 lines in english
+// 7. list of things that are asynchronous in js/ browser
+// 8. what is event loop in js and how does it work?
