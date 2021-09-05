d00b41705205720ccc3836e9197d7e17b3600d1a
mehesultana <sultanamehe01@gmail.com>
2021-09-06 02:10:45 +0600

    solved

--- 38.5-browser-api-practice/browserPractice.js
+++ 38.5-browser-api-practice/browserPractice.js
@@ -6,11 +6,20 @@ console.log('I need a tourrrr :)');
 setTimeout(() => console.log('aww jaiii *>_<*'), 3500);
 console.log('volagonj niya geleo colbeee :)');
 
-// 2. take a number using prompt . add 200 . then alert the result
+/* 2. take a number using prompt . add 200 . then alert the 
+ans: prompt.html & prompt.js
 
-// 3. do you want to see the href. if ok console log href
-// 4. purpose of cookies. 5-7 lines in english
-// 5. 3 different between local storage and session storage
+3. do you want to see the href. if ok console log href
+ans: confirm.html & confirm.js
+
+4. purpose of cookies. 5-7 lines in 
+
+=> Cookies are created to identify you when you visit a new website. The web server — which stores the website's data — sends a short stream of identifying info to your web browser. Browser cookies are identified and read by “name-value” pairs. These tell cookies where to be sent and what data to recall.
+There are three types of computer cookies: session, persistent, and third-party. These virtually invisible text files are all very different. Each with their own mission, these cookies are made to track, collect, and store any data that companies request.
+
+
+5. 3 different between local storage and session storage
+*/
 
 // ---- HOMEWORK ----
 // 6. how javascript run ? 5-7 lines in english
