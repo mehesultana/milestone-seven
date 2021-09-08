3a567c956f5e4cbe06f7e1b23b4e3b05f49e18e3
mehesultana <sultanamehe01@gmail.com>
2021-09-09 01:03:23 +0600

    solve

--- 40.5-devtool-and-debug-practice/practice.js
+++ 40.5-devtool-and-debug-practice/practice.js
@@ -7,6 +7,12 @@
 
 - - - - - - -
 5.What is accessibility?
+
+=> Web accessibility means that websites, tools, and technologies are designed and developed so that people with disabilities can use them. More specifically, people can:
+
+* perceive, understand, navigate, and interact with the Web
+* contribute to the Web
+
 6. What is a call stack?
 7. What is the purpose of regular expression?
 8. What is the purpose of the lighthouse tab of Chrome devtool
