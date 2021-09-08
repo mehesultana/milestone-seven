84da65156dfa76dd8777cf3d738b6f1f8c76f87c
mehesultana <sultanamehe01@gmail.com>
2021-09-09 00:42:02 +0600

    practice and checklist

---
+++ 40.5-devtool-and-debug-practice/practice.js
@@ -0,0 +1,25 @@
+// 40_5-1 Debug and devtool practice and checklist
+/* 
+1. Find the documentation of console tab utilities API . and then read it . Spend 30-45 minutes.
+2.Easiest way to find out the event listener attached to a dom element.
+3. How can you break the first line of a function without actually adding a breakpoint. Rather you will use a console api utilities.
+4. go to the source tab and a conditional breakpoint
+
+- - - - - - -
+5.What is accessibility?
+6. What is a call stack?
+7. What is the purpose of regular expression?
+8. What is the purpose of the lighthouse tab of Chrome devtool
+_ - - - -  -
+
+task-1
+
+9. Simple website. You will have a input and a button.Input field will take an email.use regular expression to validate that user is inserting an email.(find the regular expression by searching online)
+
+10. If email is valid display some green color text. if not an email, show some error in red color text.
+
+task-2
+
+issue tracker
+
+*/
