e879eb3c42d0e734fb688efde9a2c9c6447150ee
mehesultana <sultanamehe01@gmail.com>
2021-09-09 00:06:56 +0600

    update

--- 38.5-browser-api-practice/browserPractice.js
+++ 38.5-browser-api-practice/browserPractice.js
@@ -18,10 +18,27 @@ ans: confirm.html & confirm.js
 There are three types of computer cookies: session, persistent, and third-party. These virtually invisible text files are all very different. Each with their own mission, these cookies are made to track, collect, and store any data that companies request.
 
 
-5. 3 different between local storage and session storage
+5. difference between local storage and session storage
+
+=> 1 . 
 */
 
 // ---- HOMEWORK ----
-// 6. how javascript run ? 5-7 lines in english
-// 7. list of things that are asynchronous in js/ browser
+/*
+
+6. how javascript run ? 5-7 lines in english
+=> JavaScript is an interpreted language where code is explained and executed at the runtime. Additionally, we know that web browsers understand HTML and CSS and converting those languages into a visual display on the screen. The part of the web browser that understands HTML and CSS is called the rendering engine. However, most browsers also have a JavaScript interpreter. That’s the part of the browser that understands JavaScript and run JavaScript programs.
+the browser's Rendering engine interacts with the "JavaScript's Interpreter" and sends the JavaScript code to the JavaScript engine for processing.
+The JavaScript engine/JavaScript Interpreter comprises both an Interpreter and a Just in Time compiler. Additionally, the overall execution of the JavaScript is a 5 step process,
+1. JavaScript Engine Loads Source Code.
+2. The interpreter starts the application.
+3. The Compiler receives code.
+4. The compiler starts optimization and compilation.
+5. The compiler incrementally optimizes the code.
+
+
+7. list of things that are asynchronous in js/ browser
+=>
+
+*/
 // 8. what is event loop in js and how does it work?
