4f678f82903547b0300128442f55be3e86405db6
mehesultana <sultanamehe01@gmail.com>
2021-09-10 02:37:31 +0600

    Setup Typescript

---
+++ 41-getting-started-with-typescript/first.ts
@@ -0,0 +1,10 @@
+/* 
+let money = 5000;
+let honey = '';
+let kony = false;
+*/
+
+let money: number = 88;
+let honey: string = 'O my sweet Heart honey';
+let funny: boolean = true;
+const age: number = 21;
