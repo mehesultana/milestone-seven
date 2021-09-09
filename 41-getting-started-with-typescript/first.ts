f53f98e5617ba4d9175b887281e153e362337c68
mehesultana <sultanamehe01@gmail.com>
2021-09-10 03:01:54 +0600

    function return type

--- 41-getting-started-with-typescript/first.ts
+++ 41-getting-started-with-typescript/first.ts
@@ -1,3 +1,5 @@
+//41-3 Basic typescript type number string boolean
+
 /* 
 let money = 5000;
 let honey = '';
@@ -7,5 +9,22 @@ let kony = false;
 let money: number = 88;
 let honey: string = 'O my sweet Heart honey';
 let funny: boolean = true;
-
 const age: number = 21;
+
+// 41-4 Set function parameter type and function return type
+
+function add(first: number, second: number): number {
+    const result = first + second;
+    return result;
+}
+const output: number = add(45, 87);
+console.log(honey);
+
+// multi purpose
+/* function add(first: number | string, second: number | string): number | string {
+    const result: string | number = first + second;
+    return result;
+}
+const output: number = add(45, 87);
+const fullName = add('Adam', 'Sand');
+*/
