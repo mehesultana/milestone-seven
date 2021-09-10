39cfcca0f397dcb292c04f44400938cd016b98ab
mehesultana <sultanamehe01@gmail.com>
2021-09-10 16:09:50 +0600

    arrow function type

---
+++ 41-getting-started-with-typescript/arrowFunction.ts
@@ -0,0 +1,8 @@
+// 41-6 Object types, type and arrow function type
+function addNew(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+const add2 = (num1: number, num2: number): number => num1 + num2;
+
+const doMath = (x: number, y: number): void => {};
