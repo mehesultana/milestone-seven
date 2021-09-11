b700dfcedc5ae39b1bcc75f3be3291af2489ba45
mehesultana <sultanamehe01@gmail.com>
2021-09-12 00:01:17 +0600

    Practice Browser Api And Debug With A Checklist

---
+++ 42.5-javascript-practice-day/practice.js
@@ -0,0 +1,38 @@
+// 42 5-1 Practice Browser Api And Debug With A Checklist
+
+/* 
+1. How JS Code is executed in browser
+2. How JS handles synchronous and asynchronous calls
+3. setTimeout, setInterval
+
+-- -- -- -- -- --
+    Browser API
+-- -- -- -- -- --
+1. alert, prompt, confirm
+2. name of the different parts of an url(url parts)
+3. how to change the url using the location api
+4. use history api to go two steps back
+5. session storage vs local storage
+6. store an object in the session storage , and access the stored object and console it as an object
+
+-- -- -- -- --
+    Debug
+-- -- -- -- --
+1. how to find element that has a specific class name 
+2. add a conditional breakpoint
+3. console log each call to a function (do not break)
+4. explore dev tool official docomentation. and find out how you could be able to find out the time required to execute a for loop for one million time.
+
+-- -- -- -- 
+    TS
+-- -- -- --
+1. difference between typescript & javascript
+2. When you should use typescript
+
+-- -- -- -- -- -- --
+Debugging practice
+-- -- -- -- -- -- --
+1. Solve the issues in the Issue tracker 
+2. Solve the issue in the slider
+
+*/
