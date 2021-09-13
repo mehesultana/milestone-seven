b3e2db431c646bc75701aa04d070d84d4b89b459
mehesultana <sultanamehe01@gmail.com>
2021-09-14 02:41:54 +0600

    click on keyboard enter with debug

---
+++ 43.5-browser-debug-bonus/mealdb/mealdb.js
@@ -0,0 +1,88 @@
+// 43_5-4 Implement Search Box click on keyboard enter with debug
+
+const searchBtn = document.getElementById('button-search');
+const searchInput = document.getElementById('search-field');
+searchInput.addEventListener('keypress', function (event) {
+    // event.preventDefault();
+    if (event.key == 'Enter') {
+        searchBtn.click();
+    }
+});
+
+document.getElementById('error-message').style.display = 'none';
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    //console.log(searchText);
+    //clear data
+    searchField.value = '';
+    document.getElementById('error-message').style.display = 'none';
+    if (searchText == '') {
+        // please write something to display
+    } else {
+        // load data
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => displaySearchResult(data.meals))
+            .catch((error) => displayError(error));
+    }
+};
+
+const displayError = (error) => {
+    document.getElementById('error-message').style.display = 'block';
+};
+
+//33-6 Display dynamic search result using bootstrap cards
+const displaySearchReasult = (meals) => {
+    //console.log(meals);
+    const searchResult = document.getElementById('search-result');
+
+    // searchResult.innerHTML = '';
+    searchResult.textContent = '';
+    if (meals.length == 0) {
+        // show no result found;
+    }
+
+    meals.forEach((meal) => {
+        // console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    });
+};
+
+//33-7 Create dynamic url based on click and display data
+
+const loadMealDetail = (mealId) => {
+    console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayMealDetail(data.meals[0]));
+};
+
+const displayMealDetail = (meal) => {
+    console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+        <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    mealDetails.appendChild(div);
+};
